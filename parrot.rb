678df424765747cca1a48ae057a2727338faa172
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-03 02:40:41 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+# This function contains a default argument, with the option to accept other arguments
+
+def parrot(word = "Squawk!")
+  puts "#{word}"
+end
+
+#line to execute code
+parrot
