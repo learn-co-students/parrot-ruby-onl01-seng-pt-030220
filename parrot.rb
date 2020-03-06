de0040151d9ea28402b274c04444f3653b356bb3
Stevi Hall <career.sh.work@gmail.com>
2020-03-06 23:04:02 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
+  puts parrot("Pretty bird!")
+  return parrot("Pretty bird!")
+end
+
