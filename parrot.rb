1ac3c6e013e3c9d5803fe9232d132fc7667cd54b
frank meyer <vtoastb@gmail.com>
2020-03-06 20:47:48 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
-# Create method `parrot` that outputs a given phrase and
+=begin # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+a = "parrot"
+b = "Squawk!"
+def 
+if
+  "parrot"
+  puts "i go #{b}"
+end
+end
+=end
+def parrot(a = "Squawk!")
+  puts "#{a}"
+  return a
+end
